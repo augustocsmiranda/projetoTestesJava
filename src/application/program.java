9e0003e84827ed195814b12ff0640c3d6f00629c
augustocsmiranda <augustocezarmiranda@gmail.com>
2021-06-23 09:42:20 -0300

    Teste implantação

--- src/application/program.java
+++ src/application/program.java
@@ -5,6 +5,8 @@ public class program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Teste PUSH Git Hub");
+		System.out.println("Teste I");
 	}
 
 }
